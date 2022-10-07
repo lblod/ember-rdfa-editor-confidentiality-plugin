0b1ceff0e67f1f4d3cbd520044536403adf0fd9b
Elena Poelman <elena.poelman@redpencil.io>
2022-10-07 09:37:24 +0200

    bump editor to 1.0.0-alpha.3

--- addon/confidentiality-plugin.js
+++ addon/confidentiality-plugin.js
@@ -1,24 +1,27 @@
 import confidentialityMarkSpec from './marks/confidentiality-mark';
 
 export default class ConfidentialityPlugin {
-  controller;
-
   get name() {
     return 'confidentiality-plugin';
   }
 
-  initialize(controller) {
-    this.controller = controller;
-    controller.registerWidget({
-      componentName: 'confidentiality-toolbar-widget',
-      desiredLocation: 'toolbarMiddle',
-      identifier: 'confidentiality-plugin/toolbar-button',
-    });
-    controller.registerWidget({
-      componentName: 'confidentiality-sidebar-widget',
-      desiredLocation: 'sidebar',
-      identifier: 'confidentiality-plugin/sidebar-widget',
-    });
-    controller.registerMark(confidentialityMarkSpec);
+  initialize(transaction, controller) {
+    transaction.registerWidget(
+      {
+        componentName: 'confidentiality-toolbar-widget',
+        desiredLocation: 'toolbarMiddle',
+        identifier: 'confidentiality-plugin/toolbar-button',
+      },
+      controller
+    );
+    transaction.registerWidget(
+      {
+        componentName: 'confidentiality-sidebar-widget',
+        desiredLocation: 'sidebar',
+        identifier: 'confidentiality-plugin/sidebar-widget',
+      },
+      controller
+    );
+    transaction.registerMark(confidentialityMarkSpec);
   }
 }
